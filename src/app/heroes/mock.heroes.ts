5af204abbcfde39d4a3e1e5b4db0cc812a2b99e3
AceKokuren <cammy.black998@gmail.com>
2023-03-08 08:14:33 +0000

    Added list of mock heroes.

---
+++ src/app/heroes/mock.heroes.ts
@@ -0,0 +1,20 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 12, superHeroName: 'Dr. Nice', alterEgo: "David", age: "34", superpowers: ["Niceness"]},
+  {id: 13, superHeroName: 'Bombasto', alterEgo: "Steve", age: "34", superpowers: ["Explosive Fist"]},
+  {id: 14, superHeroName: 'Celeritas', alterEgo: "Carrie", age: "34", superpowers: ["UNKOWN"]},
+  {id: 15, superHeroName: 'Magneta', alterEgo: "Sophie", age: "34", superpowers: ["Paint the town Red"]},
+  {id: 16, superHeroName: 'RubberMan', alterEgo: "UNKOWN", age: "34", superpowers: ["Rubber Fist", "Rubber Jump"]},
+  {id: 17, superHeroName: 'Dynama', alterEgo: "Paul", age: "34", superpowers: ["Dynamite Throw"]},
+  {id: 18, superHeroName: 'Dr. IQ', alterEgo: "Chloe", age: "34", superpowers: ["Super Intelligence"]},
+  {id: 19, superHeroName: 'Magma', alterEgo: "Fiona", age: "34", superpowers: ["Lava Flow", "Explosive Burp"]},
+  {id: 20, superHeroName: 'Tornado', alterEgo: "Harsh", age: "34", superpowers: ["Gale Force", "Twister"]}
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
